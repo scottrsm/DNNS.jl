b0906c719fea7eefcf2475d90eb4eab22bb0000c
Scott McIntire <scottrsm@gmail.com>
2024-04-03 16:38:27 -0400

    Added github doc and test actions.

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,18 @@
-using Documenter, DNNS
+using DNNS
+import Pkg
 
-makedocs(sitename="DNNS Docs")
+Pkg.add("Documenter")
+using Documenter
 
+makedocs(
+	sitename = "DNNS",
+	format = Documenter.HTML(),
+	modules = [DNNS]
+	)
+
+	# Documenter can also automatically deploy documentation to gh-pages.
+	# See "Hosting Documentation" and deploydocs() in the Documenter manual
+	# for more information.
+	deploydocs(
+		repo = "github.com/scottrsm/DNNS.jl.git"
+	)
